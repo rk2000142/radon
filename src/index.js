a6d7fd0af8c6e77e1fc43a14697a063ff5712223
rk2000142 <rohitkar40@gmail.com>
2022-06-14 16:45:55 +0530

    git demo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Rohit:pV8dG1uKu7G14EMr@cluster0.dheoetr.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
